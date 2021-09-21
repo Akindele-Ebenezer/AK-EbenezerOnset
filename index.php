7c524df9d195441bf9a29dda463c056a6815a439
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2021-09-21 15:27:25 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+	<link rel='stylesheet' href='styles.css'>
+	<meta charset='UTF-8'>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Akindele Ebenezer </title>  
+</head>
+
+<body>
+
+ <div id='load'>
+  <h2>Loading</h2>
+  <div class='load'></div>
+  <div class='load'></div>
+  <div class='load'></div>
+ </div>
+    <main>
+	<nav>
+		<h1><a href='#'>AKINDELE EBENEZER</a></h1>
+			<ul>
+				<li><a href='#pg-1'>HOME</a></li>
+				<li><a href='#main-2'>PROJECTS</a></li>
+				<li><a href='#wrapper'>SERVICES</a></li>
+				<li><a href='#pg-4'>SKILLS</a></li>
+				<li><a href='#wrap-about'>ABOUT</a></li>
+				<li><a href='#form'>CONTACT</a></li> 
+			</ul>	
+			
+			<img id='nav-icon' src='images/nav-icon.PNG' alt='nav-icon'> 		
+	</nav>
+
+<div class='clear'></div>	
+ 
+    <div id='pg-1'> 
+    </div>
+ 
+		<div id='welcome-text'> 
+			<ul>
+		    <h1> I'M AKINDELE EBENEZER</h1>
+				<li>FRONTEND DEVELOPER</li>
+				<li>PROGRAMMER</li> 
+				<li>UI/UX DESIGNER</li>
+				<li>WEB DEVELOPER</li> <br>
+			</ul>
+		</div> 
+<div class='clear'></div>
+  
+  <div id='main-2'>
+	<div id='pg-2'> 
+          <h1>PROJECTS</h1>
+	     <div id='projects'>
+	       <div id='project-1'>
+		<div class='projects'>
+			<a href='https://sola-boutique.netlify.app/' target='_blank'><img src='images/sola.PNG' alt='sola'></a>
+		</div>
+			<h2>Sola Boutique</h2>
+			<p class='p-text'>Sola Online Boutique is a website I created for my friend at work. She sells perfumes for both Men and Women with very nice Packages. Click the image above to checkout the website.</p>
+	       </div>
+
+	       <div id='project-2'>
+		<div class='projects'>
+			<a href='https://akindele-ebenezer.github.io/EBEN-nOTEBOOK/' target='_blank'><img src='images/eben.PNG' alt='eben-notebook'></a>
+		</div>
+			<h2>EBEN nOTEBOOK</h2>
+			<p class='p-text'>This is my personal project. I'll add more features to this web application. I decided to build this app for myself to prompt me with the abilty to jot down important informations and tasks I need to perform each day I go to work. I spent a couple weeks to build this app. </p>
+	       </div> 
+
+	       <div id='project-3'>
+		<div class='projects'>
+			<a href='https://akindele-ebenezer.github.io/counter-app/' target='_blank'><img src='images/counter.PNG' alt='counter-app'></a>
+		</div>
+			<h2>Counter App</h2>
+			<p class='p-text'>This is the first application I've ever programmed for myself, just for fun. It's an App that can count and save the amount of numbers diplayed. </p>
+	       </div> 
+	     </div>
+	</div>
+ 
+  <div id='wrapper'>
+    <div id='pg-3'>
+	    <h1>SERVICES</h1>
+	<div id='services'>
+		<div id='web-dev' class='services'>
+		    <img src='images/web-dev.jpg' alt='web-dev'> <br>
+		    <h2>Web Development</h2> <br>
+		    <p>I'm available to help you build and implement pleasant websites. Website creation is what I do on a daily basis, from helping you to design aesthetic properties such as layout and color, to technical considerations such as designing a website to handle a given amount of Internet traffic. My responsibilty is to design and develop websites and website applications for your business/company. </p>
+		</div>
+
+		<div id='ui' class='services'>
+		    <img src='images/ui.png' alt='ui'> <br>
+		    <h2>User Interface</h2> <br>
+		    <p>I have experience with responsive visual interface design. This means I can make your website layout look good on every devices. Before I build your website, I will provide some visual contents like the colors, animation, accessibilty which I'll use in your website. I'll show you the interaction design principles that I'll implement in the website.
+</p>
+		</div>
+
+		<div id='ux' class='services'>
+		    <img id='ux-img' src='images/ux.jpg' alt='ux'> <br> 
+		    <h2 class='ux-txt'>User Experience</h2> <br>
+		    <p class='ux-txt'>To effectively create a website that will pleasantly meet your needs, I will have to discuss some ideas of what I'm about to build, VERBALLY (on the phone) or by Visual Communication (physical). Through this process, you would know what your website is going to look like. Also, you would have the feel of how the website is going to be used by anyone who visits your site. This can also prompt me to get things done pretty quickly. </p>
+		</div>
+	</div>
+    </div> 
+    
+    <div id='pg-4'>
+      <h2>SKILLS</h2>
+       <div id='skills'>
+	<div id='skill' class='skills'>
+		<img src='images/html-image.png' alt='html'>
+		<p>HTML 5</p>	
+	</div>
+
+	<div id='skill-2' class='skills'>
+		<img src='images/css-image.png' alt='html'>
+		<p>CSS 3</p>	
+	</div>
+
+	<div id='skill-3' class='skills'>
+		<img src='images/js-image.png' alt='html'>
+		<p>JAVASCRIPT</p>	
+	</div>
+     </div>
+    </div>
+ 
+    <div id='wrap-about'>
+	<div id='about'>
+		<h3>About Me</h3>
+		<p>My name is <strong>Akindele Ebenezer Oluwatobi.</strong> I'm a <strong>Front End Developer.</strong><br><br> I build well organised and responsive websites from scratch just like this one. My hobbies are Coding, Programming, Playing Musical Instruments and Designing. <br><br>I started coding when I joined the tech industry and the journey has been superb.</p> 
+	</div>
+    </div> 
+  
+  <div id='cert-wrapper'>
+      <h2 id='cert'>CERTIFICATIONS</h2>
+    <div id='certificates'>
+	<div class='certificates'>
+		<h2>COURSERA</h2>  
+		<a href='coursera.html' target='_blank'><img src='images/coursera.PNG' alt='certificate-1'></a>
+		<p>HTML, CSS & Javascript <br> for Web Developers</p> 
+	</div>
+
+	<div class='certificates'>
+		<h2>FREE CODE CAMP</h2> 
+		<a href='free-code-camp.html' target='_blank'><img src='images/free-code-camp.png' alt='certificate-2'></a>
+		<p>Responsive Web Design</p>
+	</div>
+    </div> 
+  </div>
+ 
+    <div id='form'>
+      <form action="process.php" method='post'>
+       <h2>Message me...</h2><hr>
+       <p>I'll get back to you as soon as I can</p>
+	<div id='name'>
+		<label for='name'><strong>Name</strong></label> <br>
+		<input type='text' name='name'  placeholder='Your name...'> <br><br> 
+	</div>
+	<div id='email'>
+		<label for='email'><strong>Email</strong></label> <br>
+		<input type='email' name='email' placeholder='Email...'> <br><br>
+	</div>
+	<div id='subject'>
+		<label for='subject'><strong>Subject</strong></label> <br>
+		<input type='text' name='subject'  placeholder='Heading...'> <br><br>
+	</div>
+	<div id='message'> 
+		<textarea name='message' placeholder='Type message here...'></textarea> <br><br>
+	</div>
+	<div id='button'> 
+		<input id='send' type='submit' value='SEND'>
+	</div>
+      </form> 
+
+	<div id='get-in-touch'>
+		<div id='left'>
+			<h1>Let's get in touch</h1>
+			<p>I'm open for any suggestion or just to have a chat</p>
+			<p><img src='images/address.png'><strong>Address:</strong> LAGOS, Nigeria</p> <hr>
+			<p><img src='images/ws.png'><strong>Whatsapp:</strong> 09052757805</p><hr>
+			<p><a href='mailto:tobi.akindele@gmail.com' target='_blank'><img src='images/mail.png'><strong>Email:</strong> tobi.akindele@gmail.com </a> </p><hr>
+			<p><a href='https://www.instagram.com/ebenezer__/?hl=en' target='_blank'><img src='images/ig.jpg'><strong>Instagram:</strong> @ebenezer__ </a></p><hr>
+			<p><a href='https://www.facebook.com/akindele.ebenezer/' target='_blank'><img src='images/fb.png' id='fb'><strong>Facebook:</strong> Akindele Ebenezer </a> </p>
+		</div>
+		<div id='right'>
+
+		</div> 
+	</div>
+
+    </div>
+
+      <footer>
+	<div id='footer'>
+	 <div id='footer-nav'>
+	  <ul>
+	    <li><a href='#'>HOME</a></li>
+	    <li><a href='#wrap-about'>ABOUT</a></li>
+	    <li><a href='#pg-4'>SKILLS</a></li>
+	    <li><a href='#form'>CONTACT</a></li>
+	    <li><a href='#main-2'>PROJECTS</a></li>
+	    <li><a href='#wrapper'>SERVICES</a></li>
+	  </ul>
+	 </div>
+
+	 <div id='socila-media-links'>
+	  <ul id='socila-media-links'>
+	    <li><a href='mailto:tobi.akindele@gmail.com' target='_blank'>Gmail</a></li>
+	    <li><a href='https://github.com/Akindele-Ebenezer' target='_blank'>Github</a></li>
+	    <li><a href='https://www.linkedin.com/in/akindele-ebenezer-566a16b8/' target='_blank'>LinkedIn</a></li>
+	    <li><a href='https://www.facebook.com/akindele.ebenezer/' target='_blank'>Facebook</a></li>
+	    <li><a href='https://www.instagram.com/ebenezer__/?hl=en' target='_blank'>Instagram</a></li>
+	    <li><a href='#'>Whatsapp</a></li>
+	  </ul>
+	 </div> 
+	</div>
+      </footer>
+<div class='clear'> </div>
+</div> 
+</div>
+<p id='copy'>Copyright 2021: &copy;<strong>AKINDELE EBENEZER OLUWATOBI</strong></p>
+ 
+<div class='nav-bar-sm-screen'>
+	<ul>
+		<li><a href='#pg-1'>HOME</a></li>
+		<li><a href='#main-2'>PROJECTS</a></li>
+		<li><a href='#wrapper'>SERVICES</a></li>
+		<li><a href='#pg-4'>SKILLS</a></li>
+		<li><a href='#wrap-about'>ABOUT</a></li>
+		<li><a href='#form'>CONTACT</a></li> 
+	</ul>
+</div>
+
+</main>
+
+
+<script> 
+ 
+let load = document.querySelector('#load');
+let main = document.querySelector('main'); 
+
+function loadPage() {
+	setTimeout(() => {
+		load.style.display = 'none';
+		main.style.display = 'block';
+	}, 5000)
+}
+loadPage();
+ 
+let navIcon = document.querySelector('#nav-icon');
+let navBar_Sm_Screen = document.querySelector('.nav-bar-sm-screen');
+let navBar_Sm_Screen_Ul = document.querySelector('.nav-bar-sm-screen ul').children;
+
+navIcon.addEventListener('click', () => {
+	navBar_Sm_Screen.classList.toggle('show'); 
+});
+
+for (let i = 0; i < navBar_Sm_Screen_Ul.length; i++) {
+	navBar_Sm_Screen_Ul[i].addEventListener('click', () => {
+		navBar_Sm_Screen.classList.toggle('show'); 
+	})
+}
+
+ 
+</script>
+ 
+<script src="https://apps.elfsight.com/p/platform.js" defer></script>
+<div id='whats-app' class="elfsight-app-687e7e20-b3e3-4d41-9b43-667df4de893b"></div>
+
+</body>
+
+</html>
