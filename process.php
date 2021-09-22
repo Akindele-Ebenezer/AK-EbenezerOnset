372e772ffc18f0eb9ee9ff211e43d4fa263a06bd
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2021-09-22 07:11:28 -0700

    Update process.php

--- process.php
+++ process.php
@@ -37,7 +37,7 @@ try {
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
     $mail->Subject = $subject;
-    $mail->Body    = "From: " . $name . "<br>" . "Email: " . $email . "<br>" . $message;
+    $mail->Body    = "Email: $email" . ' ' . $message;
     $mail->AltBody = 'Bye!';
 
     $mail->send();
