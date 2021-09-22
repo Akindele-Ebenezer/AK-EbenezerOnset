61fcfc779757c128cec249a709980a8b58289b7f
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2021-09-22 01:11:27 -0700

    Update process.php

--- process.php
+++ process.php
@@ -1,4 +1,10 @@
 <?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
 //Import PHPMailer classes into the global namespace
 //These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
@@ -6,16 +12,12 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader
-require './vendor/autoload.php';
+require 'vendor/autoload.php';
 
-$name = $_POST["name"];
-$mail = $_POST["email"];
-$subject = $_POST["subject"];
-$message = $_POST["message"];
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
 try {
-//Create an instance; passing `true` enables exceptions
-	$mail = new PHPMailer(true);
     //Server settings
     //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
@@ -23,22 +25,26 @@ try {
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'tobi.akindele@gmail.com';                     //SMTP username
     $mail->Password   = 'ebenezer';                               //SMTP password
-    $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('tobi.akindele@gmail.com', 'Ebenezer');
-    $mail->addAddress('tobi.akindele@gmail.com', 'Ebenezer');  
+    $mail->setFrom($email, $name);
+    $mail->addAddress('tobi.akindele@gmail.com', 'Ebenezer');     //Add a recipient
+    $mail->addAddress('tobi.akindele@gmail.com');               //Name is optional
+    $mail->addReplyTo('tobi.akindele@gmail.com', 'Information'); 
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
     $mail->Subject = $subject;
     $mail->Body    = $message;
-    $mail->AltBody = 'Stay Bleesed!';
+    $mail->AltBody = 'Bye!';
 
     $mail->send();
-		echo 'Message has been sent';
-} 	catch (Exception $e) {
-		echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-	}
+    echo "<p style='font-family: verdana; font-style: italic'>Message has been sent.</p> <hr>";
+	echo "<p style='font-family: verdana'>Thanks for your time, " . '<strong>' . strtoupper($name) . '</strong>' . '.' . " I'll get back to you.</p> <hr>";
+	echo "<p style='font-family: verdana'><a href='index.php'><strong>Go back to my SITE.</strong></a></p>";
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 ?>
