7f7e9ba90ffc0ff053a22cc0ed554025fa6ce5a6
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2021-09-22 01:52:40 -0700

    Update process.php

--- process.php
+++ process.php
@@ -24,7 +24,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'tobi.akindele@gmail.com';                     //SMTP username
-    $mail->Password   = 'ebenezer';                               //SMTP password
+    $mail->Password   = 'ebenkeyz1*';                               //SMTP password
     $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
